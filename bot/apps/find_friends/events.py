ba865d9d557ca247d7198ff05be3dec189a9ba91
An3loN <93935358+An3loN@users.noreply.github.com>
2024-07-22 16:29:47 +0300

    Feat/find friends (#5)
    * git ignore
    
    * added channel checking
    
    * lint

---
+++ bot/apps/find_friends/events.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+from bot.apps.users.utils import get_member_locale
+from bot.core.localization import LocaleEnum, LocalizationDict
+from bot.dynamic_settings import dynamic_settings
+
+
+class FindFriendEvents(commands.Cog):
+    respond_localization = LocalizationDict(
+        {
+            LocaleEnum.en: 'Use `/friend` command to find a friend',
+            LocaleEnum.ru: 'Для поиска друга воспользуйтесь командой `/friend`',
+        }
+    )
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            return
+        locale = get_member_locale(message.author)
+        if not message.channel.id in list(dynamic_settings.find_friend_channels.values()):
+            return
+        await message.delete()
+        await message.author.send(self.respond_localization[locale])
