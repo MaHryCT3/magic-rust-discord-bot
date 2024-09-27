aaf7a9aefb25dd4f2bd39478405e15ec7d880dc5
An3loN <93935358+An3loN@users.noreply.github.com>
2024-09-27 23:51:01 +0300

    Fix/image generator (#42)
    * fixed image_generator
    
    * readme fix
    
    * fixed server filter
    
    * lint ebaniy opyat' naebal

--- image_generator/__main__.py
+++ image_generator/__main__.py
@@ -1,3 +1,7 @@
-from image_generator.application import start_generation
+import sentry_sdk
 
+from image_generator.application import start_generation
+from image_generator.config import settings
+
+sentry_sdk.init(settings.SENTRY_DSN)
 start_generation()
