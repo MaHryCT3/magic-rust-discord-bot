eb7c14b24e20ce39690bca35aa1decbf1f23762e
velizhanin_ie <velizhanin_ie@its.dev>
2025-04-15 11:17:41 +0500

    fixics tasks

--- bot/apps/tickets/setup.py
+++ bot/apps/tickets/setup.py
@@ -1,7 +1,9 @@
 from bot.bot import MagicRustBot
 
 from .commands import CommandsTicketsCog
+from .tasks import TasksCog
 
 
 def setup(bot: MagicRustBot):
     bot.add_cog(CommandsTicketsCog(bot))
+    bot.add_cog(TasksCog(bot))
